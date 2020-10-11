4e2971a4b894f0f2d9cb174e062cb063df5198c5
hao wang <446941787@qq.com>
2020-10-11 17:31:43 +0800

    update

--- src/store/actionCreateors.js
+++ src/store/actionCreateors.js
@@ -1,5 +1,5 @@
 import{CHANGE_INPUT,ADD_ITEM,DELETE_ITEM_VALUE,GET_LIST,GET_MY_LIST} from './actionTypes'
-//import axios from 'axios'
+import axios from 'axios'
 
 export const changeInputAction = (value)=>({
     type:CHANGE_INPUT,
@@ -20,29 +20,19 @@ export const getListAction = (data)=>({
     data
 })
 
-// export const getTodoList = ()=>{
-//     return (dispatch)=>{
-//         axios.post('http://localhost:8001/payment/hystrix/ok').then(
-//             (res)=>{
-//                 console.log(res)
-//                 const data = res.data
-//                 const action = getListAction(data)
-//                 dispatch(action)
-//             }
-//         )
-//     }
-// }
+export const getTodoList = ()=>{
+    return (dispatch)=>{
+        axios.post('http://localhost:8001/payment/hystrix/ok').then(
+            (res)=>{
+                console.log(res)
+                const data = res.data
+                const action = getListAction(data)
+                dispatch(action)
+            }
+        )
+    }
+}
 
 export const getMyListAction = ()=>({
     type:GET_MY_LIST
-    // return (dispatch)=>{
-    //     axios.post('http://localhost:8001/payment/hystrix/ok').then(
-    //         (res)=>{
-    //             console.log(res)
-    //             const data = res.data
-    //             const action = getListAction(data)
-    //             dispatch(action)
-    //         }
-    //     )
-    // }
 })
